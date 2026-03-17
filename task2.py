c12645c2c9c4e6abd1accc016eb94a59b9dbcdcb
patharesameeksha20 <patharesameeksha20@gmail.com>
2026-03-17 12:41:00 +0100

    Add integer check to task2.py

--- task2.py
+++ task2.py
@@ -8,9 +8,9 @@ specificity_list = [0.99, 0.999, 0.9999, 0.99999]
 prevalence_list = np.geomspace(0.00001, 0.5, 300)
 
 def calculate_ppv(prevalence, sensitivity, specificity):
-    true_positive = sensitivity * prevalence
-    false_positive = (1 - specificity) * (1 - prevalence)
-    ppv = true_positive / (true_positive + false_positive)
+    true_positive_rate = sensitivity * prevalence
+    false_positive_rate = (1 - specificity) * (1 - prevalence)
+    ppv = true_positive_rate / (true_positive_rate + false_positive_rate)
     return ppv
 
 fig = go.Figure()
@@ -41,3 +41,31 @@ fig.update_xaxes(type="log")
 fig.update_yaxes(range=[0, 100])
 
 fig.show()
+
+print("Integer check for prevalence = 5% and population = 100000")
+print()
+
+population = 100000
+prevalence = 0.05
+
+infected = int(population * prevalence)
+not_infected = population - infected
+
+for specificity in specificity_list:
+    true_positives = int(infected * sensitivity)
+    false_negatives = infected - true_positives
+
+    true_negatives = int(not_infected * specificity)
+    false_positives = not_infected - true_negatives
+
+    ppv = true_positives / (true_positives + false_positives)
+
+    print("Specificity:", specificity)
+    print("Infected:", infected)
+    print("Not infected:", not_infected)
+    print("True positives:", true_positives)
+    print("False negatives:", false_negatives)
+    print("True negatives:", true_negatives)
+    print("False positives:", false_positives)
+    print("PPV:", round(ppv * 100, 2), "%")
+    print()
